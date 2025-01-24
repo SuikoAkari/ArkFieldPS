aa9efe2147c037480a7211d15a4faac48027002f
AlessandroCH <zombiered93@gmail.com>
2025-01-24 18:06:38 +0100

    1.0.3 ready

--- EndFieldPS/ConfigFile.cs
+++ EndFieldPS/ConfigFile.cs
@@ -13,6 +13,9 @@ namespace EndFieldPS
         public int DispatchPort = 5000;
         public string DispatchIp = "127.0.0.1";
         public int MaxClients = 20;
-        public bool QuestEnabled = false; 
+        public bool QuestEnabled = false; //Not used
+
+        public string MongoDb_connectionUri = "mongodb://localhost:27017";
+        public string MongoDb_databaseName = "endfieldps";
     }
 }
