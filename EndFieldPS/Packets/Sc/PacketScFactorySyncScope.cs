d82f6077e4a7d9b83bbf998cab37ba1143d18e6d
Izumi <shimizuizumi@sakurasoft.de>
2025-01-26 12:45:59 +0100

    Minor adjustment to ScFactorySyncScope

--- EndFieldPS/Packets/Sc/PacketScFactorySyncScope.cs
+++ EndFieldPS/Packets/Sc/PacketScFactorySyncScope.cs
@@ -7,12 +7,27 @@ namespace EndFieldPS.Packets.Sc
     {
 
         public PacketScFactorySyncScope(Player client) {
+            // TODO: Remove hardcoded values
             SetData(ScMessageId.ScFactorySyncScope, new ScFactorySyncScope
             {
                 ScopeName = 1,
                 CurrentChapterId = "domain_1",
                 Quickbars =
                 {
+                    new ScdFactorySyncQuickbar
+                    {
+                        List =
+                        {
+                            "item_port_power_pole_2",
+                            "item_port_travel_pole_1",
+                            "item_port_power_diffuser_1",
+                            "item_port_grinder_1",
+                            "item_port_battle_turret_1",
+                            "item_port_miner_2",
+                            "item_port_miner_3",
+                            "item_port_soil_moss_1"
+                        }
+                    },
                     new ScdFactorySyncQuickbar
                     {
                         Type = 1,
