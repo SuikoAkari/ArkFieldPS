fe080a613f0e1b20a26b28f6a1b080461bc9f7ba
AlessandroCH <zombiered93@gmail.com>
2025-01-27 16:14:02 +0100

    1.0.6 ready

--- EndFieldPS/Server.cs
+++ EndFieldPS/Server.cs
@@ -50,7 +50,7 @@ namespace EndFieldPS
             public delegate void HandlerDelegate(string command, string[] args, Player target);
         }
         public static List<Player> clients = new List<Player>();
-        public static string ServerVersion = "1.0.6-dev";
+        public static string ServerVersion = "1.0.6";
         public static bool Initialized = false;
         public static bool showLogs = true;
         public static SQLiteConnection _db;
