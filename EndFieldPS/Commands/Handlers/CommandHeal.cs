91cf7fad9615e9d966d6d3bc5be5408bbd8e33c8
AlessandroCH <zombiered93@gmail.com>
2025-01-28 17:54:35 +0100

    Created separated class for some command handlers

---
+++ EndFieldPS/Commands/Handlers/CommandHeal.cs
@@ -0,0 +1,26 @@
+﻿using EndFieldPS.Game.Entities;
+using EndFieldPS.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndFieldPS.Commands.Handlers
+{
+    public static class CommandHeal
+    {
+
+        [Server.Command("heal", "heal target current team", true)]
+        public static void HealCmd(string cmd, string[] args, Player target)
+        {
+            target.sceneManager.GetCurScene().entities.FindAll(e => e is EntityCharacter).ForEach(e =>
+            {
+                EntityCharacter chara = (EntityCharacter)e;
+                chara.GetChar().curHp = 0;
+                chara.Heal(chara.GetChar().CalcAttributes()[AttributeType.MaxHp]);
+            });
+            Logger.Print("Healed!");
+        }
+    }
+}
---
+++ EndFieldPS/Commands/Handlers/CommandHelp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndFieldPS.Commands.Handlers
+{
+    public static class CommandHelp
+    {
+        [Server.Command("help", "Show list of commands", false)]
+        public static void Handle(string cmd, string[] args, Player target)
+        {
+            Logger.Print("List of possible commands: ");
+            foreach (var command in CommandManager.s_notifyReqGroup)
+            {
+                Logger.Print($"/{command.Key} - {command.Value.Item1.desc} (Require Target: {command.Value.Item1.requiredTarget})");
+            }
+
+        }
+    }
+}
