044a94b1902471c252452fae02169a512ec2a2bc
AlessandroCH <zombiered93@gmail.com>
2025-02-01 18:49:49 +0100

    improved add cmd

---
+++ EndFieldPS/Packets/Sc/PacketScCharBagAddChar.cs
@@ -0,0 +1,28 @@
+﻿using EndFieldPS.Game.Character;
+using EndFieldPS.Network;
+using EndFieldPS.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndFieldPS.Packets.Sc
+{
+    public class PacketScCharBagAddChar : Packet
+    {
+
+        public PacketScCharBagAddChar(Player client,Character chara) {
+
+            ScCharBagAddChar proto = new ScCharBagAddChar()
+            {
+                Char = chara.ToProto(),
+                ScopeName=1,
+            };
+
+            SetData(ScMessageId.ScCharBagAddChar, proto);
+        }
+
+    }
+}
