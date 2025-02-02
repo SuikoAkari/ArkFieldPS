b63ea8add46f9086c01bae9766c4cbbb96529eb5
Xannix246 <62834423+Xannix246@users.noreply.github.com>
2025-02-02 15:20:41 +0300

    some fixes
    a bit improved remove command and wrote about cmd in docs

--- EndFieldPS/Commands/Handlers/CommandRemove.cs
+++ EndFieldPS/Commands/Handlers/CommandRemove.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using EndFieldPS.Database;
 using EndFieldPS.Game.Character;
+using EndFieldPS.Packets.Sc;
 
 namespace EndFieldPS.Commands.Handlers;
 
@@ -13,19 +14,23 @@ public static class CommandRemove
         [Server.Command("remove", "Removes character", true)]
         public static void Handle(Player sender, string cmd, string[] args, Player target)
         {
-            if(args.Length < 1) {
+            if(args.Length < 1) 
+            {
                 CommandManager.SendMessage(sender, "Use: /remove (char id)");
                 return;
             }
             
             Character character = target.chars.Find(c => c.id == args[0]);
-            if(character == null) {
+            if(character == null) 
+            {
                 CommandManager.SendMessage(sender, "Character not found");
                 return;
             }
+
             target.chars.Remove(character);
             DatabaseManager.db.DeleteCharacter(character);
             target.SaveCharacters();
-            CommandManager.SendMessage(sender, $"Character {character.id} removed. You can use /kick command to reload session");
+            target.Send(new PacketScCharBagDelChar(target, character));
+            CommandManager.SendMessage(sender, $"Character {character.id} removed.");
         }
 }
